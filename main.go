af579f07a1329b53177a67df4a1488350979873c
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-01-23 22:10:32 +0100

    rebase

--- main.go
+++ main.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"key-value-engine/structs/cms"
-)
-
 func main() {
-	cms := cms.MakeCMS(0.1, 0.1)
-	cms.Add([]byte("Luka"))
-	cms.Add([]byte("Marija"))
-	cms.Add([]byte("Ivana"))
-	cms.Add([]byte("Luka"))
-	cms.Add([]byte("Luka"))
-	cms.Add([]byte("Luka"))
-	fmt.Println(cms.Estimate([]byte("Luka")))
-	fmt.Println(cms.Estimate([]byte("Ivana")))
-	fmt.Println(cms.Estimate([]byte("Marija")))
-
 }
