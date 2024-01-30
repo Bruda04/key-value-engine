0179fb0a8853af9ce5a069668a437d8dc6926f7f
marija.parezanin@mensa.ba <marija.parezanin@mensa.ba>
2024-01-30 18:24:37 +0100

    Bloomfilter - added variable encoding

--- structs/bloomFilter/BloomFilter.go
+++ structs/bloomFilter/BloomFilter.go
@@ -1,8 +1,6 @@
 package bloomFilter
 
 import (
-	"encoding/binary"
-	"errors"
 	"math"
 )
 
@@ -77,62 +75,101 @@ func (b *BloomFilter) IsPresent(element []byte) bool {
 }
 
 func (bf *BloomFilter) BloomFilterToBytes() []byte {
-	elNumBytes := make([]byte, EL_NUM_SIZE)
-	binary.LittleEndian.PutUint64(elNumBytes, bf.elNum)
+	var result []byte
 
-	hashNumBytes := make([]byte, HASH_NUM_SIZE)
-	binary.LittleEndian.PutUint64(hashNumBytes, bf.hashNum)
+	// Encode element number using variable-length encoding
+	elNumBytes := encodeVarUint64(bf.elNum)
+	result = append(result, elNumBytes...)
 
-	sizeBytes := make([]byte, SIZE_SIZE)
-	binary.LittleEndian.PutUint64(sizeBytes, bf.size)
+	// Encode hash number using variable-length encoding
+	hashNumBytes := encodeVarUint64(bf.hashNum)
+	result = append(result, hashNumBytes...)
 
-	var hashFuncBytes []byte
+	// Encode size using variable-length encoding
+	sizeBytes := encodeVarUint64(bf.size)
+	result = append(result, sizeBytes...)
+
+	// Encode hash functions
 	for _, hf := range bf.hashFunctions {
-		seedSizeBytes := make([]byte, SEED_SIZE_SIZE)
-		binary.LittleEndian.PutUint64(seedSizeBytes, uint64(len(hf.Seed)))
-		hashFuncBytes = append(hashFuncBytes, seedSizeBytes...)
-		hashFuncBytes = append(hashFuncBytes, hf.Seed...)
+		// Encode seed size using variable-length encoding
+		seedSizeBytes := encodeVarUint64(uint64(len(hf.Seed)))
+		result = append(result, seedSizeBytes...)
+
+		// Append seed
+		result = append(result, hf.Seed...)
 	}
 
-	result := append(elNumBytes, hashNumBytes...)
-	result = append(result, sizeBytes...)
-	result = append(result, hashFuncBytes...)
+	// Append data
 	result = append(result, bf.data...)
 
 	return result
 }
 
+// Helper function to encode uint64 as variable-length bytes
+func encodeVarUint64(value uint64) []byte {
+	var encoded []byte
+	for value >= 0x80 {
+		encoded = append(encoded, byte(value)|0x80)
+		value >>= 7
+	}
+	encoded = append(encoded, byte(value))
+	return encoded
+}
+
 func BytesToBloomFilter(data []byte) (*BloomFilter, error) {
-	if len(data) < BLOOM_FILTER_HEADER_SIZE {
-		return nil, errors.New("insufficient data for BloomFilter header")
-	}
+	bf := &BloomFilter{}
 
-	elNum := binary.LittleEndian.Uint64(data[EL_NUM_START:HASH_NUM_START])
-	hashNum := binary.LittleEndian.Uint64(data[HASH_NUM_START:SIZE_START])
-	size := binary.LittleEndian.Uint64(data[SIZE_START:HASH_FUNC_START])
+	// Decode element number
+	elNum, bytesRead := decodeVarUint64(data)
+	bf.elNum = elNum
+	data = data[bytesRead:]
 
-	if len(data) < int(BLOOM_FILTER_HEADER_SIZE+size) {
-		return nil, errors.New("insufficient data for BloomFilter payload")
-	}
+	// Decode hash number
+	hashNum, bytesRead := decodeVarUint64(data)
+	bf.hashNum = hashNum
+	data = data[bytesRead:]
 
+	// Decode size
+	size, bytesRead := decodeVarUint64(data)
+	bf.size = size
+	data = data[bytesRead:]
+
+	// Decode hash functions
 	var hashFunctions []HashWithSeed
-	index := HASH_FUNC_START
 	for i := 0; i < int(hashNum); i++ {
-		seedSize := binary.LittleEndian.Uint64(data[index : index+SEED_SIZE_SIZE])
-		index += SEED_SIZE_SIZE
-		seed := data[index : index+int(seedSize)]
-		index += int(seedSize)
+		// Decode seed size
+		seedSize, bytesRead := decodeVarUint64(data)
+		data = data[bytesRead:]
+
+		// Extract seed
+		seed := data[:seedSize]
+		data = data[seedSize:]
 
 		hashFunctions = append(hashFunctions, HashWithSeed{Seed: seed})
 	}
+	bf.hashFunctions = hashFunctions
 
-	bf := &BloomFilter{
-		elNum:         elNum,
-		hashNum:       hashNum,
-		size:          size,
-		hashFunctions: hashFunctions,
-		data:          data[index:],
-	}
+	// The remaining data is the Bloom filter data
+	bf.data = data
 
 	return bf, nil
+}
+
+// Helper function to decode variable-length uint64 from byte slice
+func decodeVarUint64(data []byte) (uint64, int) {
+	var result uint64
+	var bytesRead int
+	for shift := 0; shift < 64; shift += 7 {
+		if len(data) == 0 {
+			break
+		}
+		b := data[0]
+		data = data[1:]
+		result |= (uint64(b) & 0x7F) << shift
+		bytesRead++
+		if b&0x80 == 0 {
+			break
+		}
+	}
+	return result, bytesRead
 }
